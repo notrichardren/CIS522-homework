3d3bf4ca8445ff7c1cc3e749a6a3ba9a81efe5ab
Richard Ren <34405553+notrichardren@users.noreply.github.com>
2023-01-31 21:37:50 -0500

    Formatted & type checked everything.

--- assignments/week1/model.py
+++ assignments/week1/model.py
@@ -2,6 +2,9 @@ import numpy as np
 
 
 class LinearRegression:
+    """
+    A linear regression model that uses an analytical solution to fit the model. Bias not implemented because Ed says it was OK to do so.
+    """
 
     w: np.ndarray
     b: float
@@ -10,16 +13,36 @@ class LinearRegression:
         self.w = np.zeros((2, 2))
         self.b = np.zeros((2, 2))
 
-    def fit(self, X, y):
+    def fit(self, X: np.ndarray, y: np.ndarray) -> None:
+        """
+        Fit the model.
+
+        Arguments:
+            X (np.ndarray): The input data.
+            Y (np.ndarray): The output data.
+
+        Returns:
+            None
+        """
+
         if np.linalg.det(X.T @ X) != 0:
             self.w = (np.linalg.inv(X.T @ X)) @ (X.T @ y)
         else:
             print(
                 "Closed form solution not possible here as determinant of X.T@X is not zero"
             )
-            return None
 
-    def predict(self, X):
+    def predict(self, X: np.ndarray) -> np.ndarray:
+        """
+        Predict the output for the given input.
+
+        Arguments:
+            X (np.ndarray): The input data.
+
+        Returns:
+            np.ndarray: The predicted output.
+
+        """
         self.b = np.zeros((X.shape[0],))
         preds = (X @ self.w.T) + self.b
         return preds
@@ -27,12 +50,23 @@ class LinearRegression:
 
 class GradientDescentLinearRegression(LinearRegression):
     """
-    A linear regression model that uses gradient descent to fit the model.
+    A linear regression model that uses gradient descent to fit the model. Bias not implemented because Ed says it was OK to do so.
     """
 
     def fit(
         self, X: np.ndarray, y: np.ndarray, lr: float = 0.01, epochs: int = 1000
     ) -> None:
+        """
+        Fit the model.
+
+        Arguments:
+            X (np.ndarray): The input data.
+            Y (np.ndarray): The output data.
+            lr (float): The learning rate.
+            epochs (int): The number of epochs.
+        Returns:
+            None
+        """
 
         np.random.seed(42)
         self.w = np.random.randn(X.shape[1], 1)  # initial weights
