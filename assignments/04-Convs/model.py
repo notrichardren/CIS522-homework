1010057c346b658b573151d2ea4b30a11c279a20
Richard Ren <34405553+notrichardren@users.noreply.github.com>
2023-03-19 00:00:48 -0400

    hi

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -4,54 +4,10 @@ import torch
 # https://pytorch.org/tutorials/beginner/blitz/cifar10_tutorial.html
 # https://en.wikipedia.org/wiki/LeNet
 
+# 35.5 seconds with 55% accuracy.
 
-# class Model(torch.nn.Module):
-#     def __init__(self, num_channels: int, num_classes: int) -> None:
-#         super().__init__()
-
-#         # (32, 32, 3)
-#         self.conv1 = torch.nn.Conv2d(
-#             in_channels=num_channels,
-#             out_channels=10,
-#             kernel_size=5,
-#             padding=2,
-#             stride=1,
-#         )
-#         # (32, 32, 3)
-#         self.batchnorm1 = torch.nn.BatchNorm2d(10)
-#         self.relu1 = torch.nn.ReLU()
-#         self.maxpool1 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
-#         # (16, 16, 10)
-
-#         # (16, 16, 10)
-#         self.conv2 = torch.nn.Conv2d(in_channels=10, out_channels=20, kernel_size=5)
-#         # (12, 12, 20)
-#         self.batchnorm2 = torch.nn.BatchNorm2d(20)
-#         self.relu2 = torch.nn.ReLU()
-#         self.maxpool2 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
-#         # (6, 6, 20)
-
-#         self.fc1 = torch.nn.Linear(720, 120)
-#         self.relu3 = torch.nn.ReLU()
-#         self.fc2 = torch.nn.Linear(120, num_classes)
-
-#     def forward(self, x: torch.Tensor) -> torch.Tensor:
-#         x = self.conv1(x)
-#         x = self.batchnorm1(x)  # 10, 32, 32
-#         x = self.relu1(x)
-#         x = self.maxpool1(x)  # 10, 16, 16
-
-#         x = self.conv2(x)
-#         x = self.batchnorm2(x)
-#         x = self.relu2(x)
-#         x = self.maxpool2(x)  #
-
-#         x = torch.flatten(x, start_dim=1)  # 20, 6, 6
-
-#         x = self.fc1(x)
-#         x = self.relu3(x)
-#         x = self.fc2(x)
-#         return x
+batch_size = 100
+num_epochs = 8
 
 
 class Model(torch.nn.Module):
@@ -64,7 +20,7 @@ class Model(torch.nn.Module):
         # (32, 32, 3)
         self.conv1 = torch.nn.Conv2d(
             in_channels=num_channels,
-            out_channels=15,
+            out_channels=32,
             kernel_size=3,
             padding=1,
             stride=1,
@@ -75,13 +31,12 @@ class Model(torch.nn.Module):
         # (16, 16, 8)
 
         # (16, 16, 8)
-        self.conv2 = torch.nn.Conv2d(in_channels=15, out_channels=40, kernel_size=3)
+        self.conv2 = torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3)
         # (14, 14, 16)
         self.relu2 = torch.nn.ReLU()
         self.maxpool2 = torch.nn.MaxPool2d(kernel_size=2, stride=2)
         # (7, 7, 16)
-
-        self.fc1 = torch.nn.Linear(7 * 7 * 40, num_classes)
+        self.fc1 = torch.nn.Linear(7 * 7 * 64, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
